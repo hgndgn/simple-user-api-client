1313f0629c5cfca966ec45d0555326fca9fc35e9
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-20 22:07:20 +0100

    routing added

--- simple-user-api-client/src/app/app.module.ts
+++ simple-user-api-client/src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { UserService } from './service/user.service';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
-import { UserComponent } from './user/user.component';
-import { UsersComponent } from './users/users.component';
+import {UserService} from './service/user.service';
+import {UserComponent} from './user/user.component';
+import {UsersComponent} from './users/users.component';
 
 
 @NgModule({
-  declarations: [AppComponent,
-    UserComponent,
-    UsersComponent
-],
-  imports: [BrowserModule, HttpModule],
+  declarations: [AppComponent, UserComponent, UsersComponent],
+  imports: [
+    BrowserModule, HttpModule, RouterModule.forRoot([
+      {path: 'users/:username', component: UserComponent},
+      {path: 'users', component: UsersComponent}
+    ])
+  ],
   providers: [UserService],
   bootstrap: [AppComponent]
 })
