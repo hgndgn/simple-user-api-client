b795e6856fe456800adf8403afd2e0f0f66eed63
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-21 18:04:31 +0100

    view udpated and module added

--- simple-user-api-client/src/app/app.module.ts
+++ simple-user-api-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {UserService} from './service/user.service';
@@ -12,7 +13,7 @@ import {UsersComponent} from './users/users.component';
 @NgModule({
   declarations: [AppComponent, UserComponent, UsersComponent],
   imports: [
-    BrowserModule, HttpModule, RouterModule.forRoot([
+    BrowserModule, HttpModule, FormsModule, RouterModule.forRoot([
       {path: 'users/:username', component: UserComponent},
       {path: 'users', component: UsersComponent},
       {path: '**', redirectTo: 'users'}
